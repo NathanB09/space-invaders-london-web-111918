b7fc68f16d157a071cadac733cc4308daa7bd0b5
NathanB09 <nathanbell09@gmail.com>
2019-01-13 16:27:37 +0000

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,28 @@
-class Spaceship {}
+const store = {
+  spaceships: [],
+  crewMembers: []
+}
+
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    this.phasersCharge = 'uncharged'
+
+    if (this.crew.length > 0) {
+      this.docked = false
+      for (const member of this.crew) {
+        member.currentShip = this
+      }
+    } else {
+      this.docked = true
+    }
+
+    store.spaceships.push(this)
+
+  }
+}
